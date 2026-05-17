67c5fe7255260230e983b32956451eb74962ec7f
coco <9626471@gmail.com>
2026-05-17 12:51:05 +0800

    Add backend auto deploy workflow

---
+++ scripts/deploy-backend.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+set -eu
+
+APP_DIR="${APP_DIR:-/opt/chatty}"
+BRANCH="${BRANCH:-main}"
+COMPOSE_FILE="${COMPOSE_FILE:-docker-compose.prod.yml}"
+COMPOSE_PROJECT_NAME="${COMPOSE_PROJECT_NAME:-chatty}"
+BACKEND_SERVICE="${BACKEND_SERVICE:-chatty-backend}"
+COMPOSE="docker compose -p $COMPOSE_PROJECT_NAME -f $COMPOSE_FILE"
+
+cd "$APP_DIR"
+
+git fetch origin "$BRANCH"
+git reset --hard "origin/$BRANCH"
+
+$COMPOSE build "$BACKEND_SERVICE"
+$COMPOSE run --rm "$BACKEND_SERVICE" npm run prisma:deploy
+$COMPOSE up -d "$BACKEND_SERVICE"
+docker image prune -f
