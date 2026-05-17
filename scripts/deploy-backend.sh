231d63ea488ca24433ca0bc0a9bc30744509cc6c
coco <9626471@gmail.com>
2026-05-17 14:52:36 +0800

    Deploy frontend with app workflow

--- scripts/deploy-backend.sh
+++ scripts/deploy-backend.sh
@@ -6,6 +6,7 @@ BRANCH="${BRANCH:-main}"
 COMPOSE_FILE="${COMPOSE_FILE:-docker-compose.prod.yml}"
 COMPOSE_PROJECT_NAME="${COMPOSE_PROJECT_NAME:-chatty}"
 BACKEND_SERVICE="${BACKEND_SERVICE:-chatty-backend}"
+FRONTEND_SERVICE="${FRONTEND_SERVICE:-chatty-frontend}"
 COMPOSE="docker compose -p $COMPOSE_PROJECT_NAME -f $COMPOSE_FILE"
 
 cd "$APP_DIR"
@@ -13,7 +14,7 @@ cd "$APP_DIR"
 git fetch origin "$BRANCH"
 git reset --hard "origin/$BRANCH"
 
-$COMPOSE build "$BACKEND_SERVICE"
+$COMPOSE build "$BACKEND_SERVICE" "$FRONTEND_SERVICE"
 $COMPOSE run --rm "$BACKEND_SERVICE" npm run prisma:deploy
-$COMPOSE up -d --force-recreate --no-deps "$BACKEND_SERVICE"
+$COMPOSE up -d --force-recreate "$BACKEND_SERVICE" "$FRONTEND_SERVICE"
 docker image prune -f
